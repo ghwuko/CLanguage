efc0462e6598e0e973482ac372a730147e98548d
wqb-why <1744869760@qq.com>
2021-07-22 20:07:57 +0800

    初识指针

--- C语言上课练习/test1_7_22/test1_7_22/test.c
+++ C语言上课练习/test1_7_22/test1_7_22/test.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#define NUM 5
+//#define NUM 5
 //int main()
 //{
 //	//float a = 3.4f;
@@ -259,20 +259,206 @@
 //}
 
 
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;//数组名  -- 首元素的地址
+//	// char* p = arr; 不能将数组全部改成1，只能将前十个字节改成1
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a; // 局部变量不初始化，默认是随机值
+//	int* p; // 局部的指针变量，就被初始化随机值
+//	// *p = 20;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 12; i++)
+//	{
+//		// 当指针指向的范围超出数组arr的范围时，p就是野指针
+//		*(p++) = i;
+//	}
+//	return 0;
+//}
+
+//int* test()
+//{
+//	int a = 10;
+//	return &a;
+//}
+//int main()
+//{
+//	int* p = test();
+//	//*p = 20;
+//	printf("%d\n", *p);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a; // 初始化
+//	int* p = NULL; // 当不知道一个指针变量该初始化什么值的时候就初始化成 NULL
+//	// NULL -- (void *)0     -- 本质上还是 0 
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int a = 10;
+//    int* pa = &a;
+//    *pa = 20;
+//    //
+//    pa = NULL;
+//    // 4指针使用之前检查有效性
+//    // *pa = 10;  // 报错
+//    if (pa != NULL)
+//    {
+//        *pa = 10;
+//    }
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//int* p = arr;
+//	//for (i = 0; i < sz; i++)
+//	//{
+//	//	printf("%d\n", *p++);
+//	//	// printf("%d\n", *p);
+//	//	// p = p + 1;
+//	//}
+//
+//	int* p = &arr[9];
+//	for (i = 0; i < ; i++)
+//	{
+//		printf("%d ", *p--);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9.10 };
+//	char ch[5] = { 0 };
+//	printf("%d\n",& arr[9] - &arr[0]); //  9    -- 指针 - 指针 得到的是中间的元素个数
+//	printf("%d\n", &arr[0] - &arr[9]); // -9
+//
+//	// printf("%d\n",& arr[9] - &ch[0]);  // 错误写法，结果不可预知
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0')
+//	{
+//		end++;
+//	}
+//	return end - start;
+//}
+//int main()
+//{
+//	// strlen  -- 求字符串长度
+//	// 模拟实现 strlen  -- 计算器的方式1，递归的方式2
+//
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//}
+
+//#define NUM 5
+//int main()
+//{
+//	float arr[NUM];
+//	float* pa;
+//	for (pa = &arr[NUM]; pa > &arr[0];)
+//	{
+//		*--pa = 0;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);  // 地址 -- 首元素的地址    // 00D3FD4C
+//	printf("%p\n", arr+1);
+//	printf("%p\n", &arr[0]);                        // 00D3FD4C
+//	printf("%p\n", &arr[0]+1);
+//	printf("%p\n", &arr); // 00D3FD4C  -- 整个数组的地址 
+//	printf("%p\n", &arr+1);
+//	// 1. &arr  -- &数组名 -- 数组名不是首元素的地址  -- 数组名表示整个数组 -- &数组名取出的是整个数组的地址
+//	// 2. sizeof(arr)  -- sizeof(数组名)  -- 数组名表示整个数组 -- 计算的是整个数组的大小
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%p ===== %p\n", p + i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;  // 一级指针
+//	int** ppa = &pa; // 二级指针
+//	// int*** pppa = &ppa; // 三级指针
+//	printf("%d\n", **ppa);
+//	**ppa = 20;
+//	printf("%d\n", **ppa);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
 int main()
 {
-	int arr[10] = { 0 };
-	int* p = arr;//数组名  -- 首元素的地址
-	// char* p = arr; 不能将数组全部改成1，只能将前十个字节改成1
-	int i = 0;
-	for (i = 0; i < 10; i++)
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	//int* pa = &a;
+	//int* pb = &b;
+	//int* pc = &c;
+	// 整型数组 -- 存放整型
+	// 字符数组  -- 存放字符
+	// 指针数组  -- 存放指针
+	// int arr[10];
+	int* arr2[3] = { &a,&b,&c }; // 指针数组
+	for (int i = 0; i < 3; i++)
 	{
-		*(p + i) = 1;
+		printf("%d ",*(arr2[i]));
 	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d\n", arr[i]);
-	}
+
 	return 0;
 }
-
