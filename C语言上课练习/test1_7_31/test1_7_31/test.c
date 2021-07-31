32faa741a29c0013e5ddf45d82b94e9c0d7c87ff
wqb-why <1744869760@qq.com>
2021-07-31 20:10:42 +0800

    .

---
+++ C语言上课练习/test1_7_31/test1_7_31/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	//printf("%d\n", sizeof(int));
+//	//printf("%d\n", sizeof(long));
+//	//printf("%d", 65);
+//	//printf("%c", 65);
+//	//float a = 3.0;
+//	//float b = 3.1; // 会报警告
+//	//char arr[5] = { 'a'};
+//	//scanf("%s", arr);
+//	//printf("(are you ok\??)\n");// (are you ok??)
+//	//printf("%s\n", "c:\test\32\test.c");
+//	//printf("%s\n", "c:\test\328\test.c");
+//	//printf("%s\n", "dsf\091");
+//	//printf("%s\n", "abc0def");// abc0def
+//	//printf("%s\n", "abc\0def");// abc
+//
+//	//printf("%d\n", strlen("abc\0def"));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int Add(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个整数:>");
+//	scanf("%d%d", &a, &b);
+//	printf("%d 和 %d 中的较大值为：%d", a, b, Add(a, b));
+//	return 0;
+//}
