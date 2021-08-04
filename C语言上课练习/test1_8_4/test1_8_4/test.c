c53462cc1c6c773e68debe994f2704f98ed68d58
wqb-why <1744869760@qq.com>
2021-08-04 20:17:54 +0800

    函数、递归练习

--- C语言上课练习/test1_8_4/test1_8_4/test.c
+++ C语言上课练习/test1_8_4/test1_8_4/test.c
@@ -1,4 +1,169 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <assert.h>
+char* my_strcpy(char* dest, const char* src)
+{
+	assert(dest != NULL);
+	assert(src != NULL);
+
+	char* ret = dest;
+	while (*dest++ = *src++)
+	{
+		;
+	}
+	return ret;
+
+}
+int main()
+{
+	char arr1[] = "xxxxxxxxxxxxxx";
+	char arr2[] = "bit";
+
+	printf("%s\n", my_strcpy(arr1, arr2));
+}
+//unsigned int my_strlen(const char* str)
+//{
+//	assert(str != NULL);
+//	int count = 0;
+//	while (*str++)
+//		count++;
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("字符个数为：%u\n", my_strlen(arr));
+//	return 0;
+//}
+//int fei1(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fei1(n - 1) + fei1(n - 2);
+//}
+//int fei2(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("递归实现：%d\n", fei1(n));
+//	printf("非递归实现：%d\n", fei2(n));
+//
+//	return 0;
+//}
+//int pow(int n, int k)
+//{
+//	if (k >= 1)
+//		return n * pow(n, k - 1);
+//	else
+//		return 1;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	printf("%d的%d次方为：%d\n", n, k, pow(n, k));
+//	return 0;
+//}
+//int DigitSum(int n)
+//{
+//	if (n > 9)
+//		return n % 10 + DigitSum(n / 10);
+//	else
+//		return n;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", DigitSum(n));
+//	return 0;
+//}
+//int my_strlen1(char* str)
+//{
+//	if (*str)
+//		return 1 + my_strlen1(str + 1);
+//	else 
+//		return 0;
+//}
+//
+//int my_strlen2(char* str)
+//{
+//	int count = 0;
+//	while (*str++)
+//		count++;
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("递归实现strlen：%d\n", my_strlen1(arr));
+//	printf("非递归实现strlen：%d\n", my_strlen2(arr));
+//
+//	return 0;
+//}
+
+
+//
+//int Fac1(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac1(n - 1);
+//}
+//
+//int Fac2(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("递归方式求%d的阶乘:%d\n", n, Fac1(n));
+//	printf("非递归方式求%d的阶乘:%d\n", n, Fac2(n));
+//
+//	return 0;
+//}
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	print(a);
+//	return 0;
+//}
+
+
 //#include <stdio.h>
 //#include <assert.h>
 //int my_strlen(const char* p)
@@ -141,9 +306,62 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-int main()
-{
+//#include <stdio.h>
+//void print(unsigned int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num); // 1234
+//	print(num);
+//	return 0;
+//}
 
-	return 0;
-}
+//#include <stdio.h>
+//#include <assert.h>
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL); // 断言
+//	assert(src != NULL);  // 断言
+//
+//	// 把 src 指向的字符串拷贝到 dest 指向的空间，包含 '\0'字符
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	// strcpy
+//	// 字符串拷贝
+//	// 1
+//	//char arr1[] = "###################";
+//	//char arr2[] = "bit";
+//
+//	// 2  -- 错误写法  -- arr2没有'\0'，循环不会停止
+//	// 	   源字符串中一定要有\0
+//	//char arr1[] = "###################";
+//	//char arr2[] = { 'a','b','c' };
+//
+//	// 3   -- 目标空间必须足够大
+//	//char arr1[3] = { 0 };
+//	//char arr2[] = "abcdef";
+//
+//	// 4  -- 目标空间必须可修改
+//	char* arr1 = "xxxxxxxxx"; // 常量字符串，是放在常量区的，是不能修改的
+//	// const char* arr1 = "xxxxxxxxx"; //严谨的写法
+//	char arr2[] = "abcdef";
+//	
+//	// 链式访问
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
+
